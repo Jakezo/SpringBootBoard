8e9d5654dfb82f74bb75b783cc5dddf3d2d10828
jake <jake@laidback.co.kr>
2021-09-19 23:19:24 +0900

    boardList

--- src/main/java/com/springboot/board/controller/BoardController.java
+++ src/main/java/com/springboot/board/controller/BoardController.java
@@ -3,24 +3,29 @@ package com.springboot.board.controller;
 import com.springboot.board.dto.BoardDto;
 import com.springboot.board.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class BoardController {
     private BoardService boardService;
 
-    public BoardController(BoardService boardService){
+    public BoardController(BoardService boardService) {
         this.boardService = boardService;
     }
 
     @GetMapping("/")
-    public String list(){
+    public String list(Model model) {
+        List<BoardDto> boardDtoList = boardService.getBoardList();
+        model.addAttribute("postList", boardDtoList);
         return "board/list.html";
     }
 
     @GetMapping("/post")
-    public String post(){
+    public String post() {
         return "board/post.html";
     }
 
