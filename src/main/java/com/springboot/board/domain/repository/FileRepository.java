726b86b939b15f211d3a973527fe9eba1f0e0109
jake <cakim21@naver.com>
2021-10-04 20:35:45 +0900

    2021-10-04

--- src/main/java/com/springboot/board/domain/repository/FileRepository.java
+++ src/main/java/com/springboot/board/domain/repository/FileRepository.java
@@ -3,5 +3,5 @@ package com.springboot.board.domain.repository;
 import com.springboot.board.domain.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<File, Long> {
+public interface FileRepository extends JpaRepository<File, Long>{
 }
